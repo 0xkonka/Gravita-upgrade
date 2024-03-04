851d0b1cf892a26a700993036e8f8b89ac912853
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-03-04 12:42:06 +0100

    🎉 Imported contracts/GasPool.sol

---
+++ contracts/GasPool.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GasPool is Ownable {
+    // do nothing, as the core contracts have permission to send to and burn from this address
+
+    string public constant NAME = "GasPool";
+
+    constructor(address initialOwner) Ownable(initialOwner) { }
+}
