38dbf458da63ad676d85a5bd5506e5d7035ebd68
0xkonka <8722069+0xkonka@users.noreply.github.com>
2024-03-04 16:26:23 +0100

    ⚰️ Remove custom impementation of ERC20Permit
    Replaced with the OpenZeppelin implementation

--- contracts/TREN/TRENToken.sol
+++ contracts/TREN/TRENToken.sol
@@ -2,16 +2,18 @@
 
 pragma solidity ^0.8.23;
 
-import { ERC20Permit, ERC20 } from "../Dependencies/ERC20Permit.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract TRENToken is ERC20Permit {
-    string public constant NAME = "TRENToken";
+    string public constant NAME = "TREN";
+    string public constant SYMBOL = "TREN";
 
     uint256 internal _1_MILLION = 1e24; // 1e6 * 1e18 = 1e24
 
     address public immutable treasury;
 
-    constructor(address _treasurySig) ERC20("TREN", "TREN") {
+    constructor(address _treasurySig) ERC20(NAME, SYMBOL) ERC20Permit(NAME) {
         require(_treasurySig != address(0), "Invalid Treasury Sig");
         treasury = _treasurySig;
 
