94cde0e12fd1fdcbce3206de4ce4081bdd564d70
Kei <kei@tren.finance>
2024-03-21 00:54:13 +0900

    owner behavior

--- test/unit/priceFeed/PriceFeed.behavior.ts
+++ test/unit/priceFeed/PriceFeed.behavior.ts
@@ -1,8 +1,14 @@
 import shouldHavePublicConstant from "./view/constants";
+import shouldBehaveLikeOwner from "./view/owner";
 
 export function shouldBehaveLikePriceFeedContract(): void {
-    describe("View Functions", function () {
-      shouldHavePublicConstant();
+  describe("View Functions", function () {
+    shouldHavePublicConstant();
+
+    describe("#owner", function () {
+      shouldBehaveLikeOwner();
     });
-    describe("Effects Functions", function () {});
+  });
+
+  describe("Effects Functions", function () {});
 }
