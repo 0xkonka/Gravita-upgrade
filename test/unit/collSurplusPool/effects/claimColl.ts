756bde68f6c20c32d6cc44309623e203d322a7f3
letteldream <letteldream@gmail.com>
2024-03-22 12:03:53 -0500

    test: update claimColl test

--- test/unit/collSurplusPool/effects/claimColl.ts
+++ test/unit/collSurplusPool/effects/claimColl.ts
@@ -4,83 +4,107 @@ import { ethers } from "hardhat";
 export default function shouldBehaveLikeCanClaimColl(): void {
   beforeEach(async function () {
     const CollSurplusPoolFactory = await ethers.getContractFactory("CollSurplusPool");
-    const redeployedCollSurplusPool = await CollSurplusPoolFactory.connect(this.signers.deployer).deploy();
-    await redeployedCollSurplusPool.waitForDeployment();
-    await redeployedCollSurplusPool.initialize();
+    const collSurplusPool = await CollSurplusPoolFactory.connect(this.signers.deployer).deploy();
+    await collSurplusPool.waitForDeployment();
+    await collSurplusPool.initialize();
 
-    this.redeployedContracts.collSurplusPool = redeployedCollSurplusPool;
+    const ActivePoolFactory = await ethers.getContractFactory("ActivePool");
+    const activePool = await ActivePoolFactory.connect(this.signers.deployer).deploy();
+    await activePool.waitForDeployment();
+    await activePool.initialize();
+
+    const TrenBoxManagerFactory = await ethers.getContractFactory("TrenBoxManager");
+    const trenBoxManager = await TrenBoxManagerFactory.connect(this.signers.deployer).deploy();
+    await trenBoxManager.waitForDeployment();
+    await trenBoxManager.initialize();
+
+    this.redeployedContracts.collSurplusPool = collSurplusPool;
+    this.redeployedContracts.activePool = activePool;
+    this.redeployedContracts.trenBoxManager = trenBoxManager;
 
     this.impostor = this.signers.accounts[1];
+
+    const { erc20 } = this.testContracts;
+    await erc20.mint(this.redeployedContracts.collSurplusPool, 1000n);
   });
 
-  context("when caller is active pool", function () {
+  context("when caller is borrower operations", function () {
     beforeEach(async function () {
-      const addressesForSetAddresses = await this.utils.getAddressesForSetAddresses({
+      const addressesForSetAddresses1 = await this.utils.getAddressesForSetAddresses({
         borrowerOperations: this.impostor,
+        activePool: this.impostor,
+        trenBoxManager: this.impostor,
       });
 
-      await this.redeployedContracts.collSurplusPool.setAddresses(addressesForSetAddresses);
+      await this.redeployedContracts.collSurplusPool.setAddresses(addressesForSetAddresses1);
     });
 
-    shouldBehaveLikeCanClaimCollCorrectly();
-  });
-
-
-  context("when caller is not active pool", function () {
     it("reverts custom error", async function () {
-      this.impostor = this.signers.accounts[1];
-      const { wETH } = this.collaterals.active;
-
-      const user = this.signers.accounts[1]
+      const { erc20 } = this.testContracts;
+      const user = this.signers.accounts[1];
 
       await expect(
-        this.contracts.collSurplusPool.connect(this.impostor).claimColl(wETH.address, user)
+        this.redeployedContracts.collSurplusPool.connect(this.impostor).claimColl(erc20, user)
+      ).to.be.revertedWith("CollSurplusPool: No collateral available to claim");
+    });
+
+    it("should claim Collateral", async function () {
+      const { erc20 } = this.testContracts;
+      const assetAmount = BigInt(10);
+      const user = this.signers.accounts[1];
+
+      const userAssetBalanceBefore = await this.redeployedContracts.collSurplusPool.getCollateral(
+        erc20,
+        user.address
+      );
+
+      const accountSurplusTx = await this.redeployedContracts.collSurplusPool
+        .connect(this.impostor)
+        .accountSurplus(erc20, user.address, assetAmount);
+
+      await expect(accountSurplusTx)
+        .to.emit(this.redeployedContracts.collSurplusPool, "CollBalanceUpdated")
+        .withArgs(user, assetAmount);
+
+      const userAssetBalanceAfter = await this.redeployedContracts.collSurplusPool.getCollateral(
+        erc20,
+        user.address
+      );
+
+      expect(assetAmount).to.be.equal(userAssetBalanceAfter - userAssetBalanceBefore);
+
+      const assetBalanceBefore =
+        await this.redeployedContracts.collSurplusPool.getAssetBalance(erc20);
+
+      await this.redeployedContracts.collSurplusPool
+        .connect(this.impostor)
+        .receivedERC20(erc20, assetAmount);
+
+      const assetBalanceAfter =
+        await this.redeployedContracts.collSurplusPool.getAssetBalance(erc20);
+
+      expect(assetAmount).to.be.equal(assetBalanceAfter - assetBalanceBefore);
+
+      const claimCollTx = await this.redeployedContracts.collSurplusPool
+        .connect(this.impostor)
+        .claimColl(erc20, user.address);
+
+      await expect(claimCollTx)
+        .to.emit(this.redeployedContracts.collSurplusPool, "CollBalanceUpdated")
+        .withArgs(user, 0);
+    });
+  });
+
+  context("when caller is not borrower operations", function () {
+    it("reverts custom error", async function () {
+      this.impostor = this.signers.accounts[1];
+      const { erc20 } = this.testContracts;
+
+      const user = this.signers.accounts[1];
+
+      await expect(
+        this.contracts.collSurplusPool.connect(this.impostor).claimColl(erc20, user)
       ).to.be.revertedWith("CollSurplusPool: Caller is not Borrower Operations");
     });
   });
-}
-
-function shouldBehaveLikeCanClaimCollCorrectly() {
-
-  it("reverts custom error", async function () {
-    const { wETH } = this.collaterals.active;
-    const user = this.signers.accounts[1]
-
-    await expect(this.redeployedContracts.collSurplusPool.connect(this.impostor).claimColl(wETH.address, user)).to.be.revertedWith("CollSurplusPool: No collateral available to claim")
-
-  });
-
-  // it("receives asset token and increase balance", async function () {
-  //   const { wETH } = this.collaterals.active;
-  //   const assetAmount = 0n;
-  //   const user = this.signers.accounts[1]
-
-  //   const assetBalanceBefore = await this.redeployedContracts.collSurplusPool.getCollateral(
-  //     wETH.address, user
-  //   );
-
-  //   await this.redeployedContracts.collSurplusPool
-  //     .connect(this.impostor)
-  //     .claimColl(wETH.address, user);
-  //   const assetBalanceAfter = await this.redeployedContracts.collSurplusPool.getCollateral(
-  //     wETH.address, user
-  //   );
-
-  //   expect(assetBalanceAfter).to.be.equal(assetBalanceBefore - assetAmount);
-  // });
-
-  // it("should emit CollBalanceUpdated", async function () {
-  //   const { wETH } = this.collaterals.active;
-  //   const assetAmount = 50n;
-  //   const user = this.signers.accounts[1]
-
-  //   const claimCollTx = await this.redeployedContracts.collSurplusPool
-  //     .connect(this.impostor)
-  //     .claimColl(wETH.address, user);
-
-  //   await expect(claimCollTx)
-  //     .to.emit(this.redeployedContracts.collSurplusPool, "CollBalanceUpdated")
-  //     .withArgs(user, assetAmount);
-  // });
-
 }
