1ab4c8031a27dc22c7ea6038ba42fffa0419c2d8
Kei <kei@tren.finance>
2024-03-22 22:04:34 +0900

    fix lint sol

--- contracts/TestContracts/MockAggregator.sol
+++ contracts/TestContracts/MockAggregator.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import { AggregatorV3Interface } from
+    "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract MockAggregator is AggregatorV3Interface {
     uint8 private decimalsVal = 8;
-    int256 private answer = 190000000000;
-    int256 private prevAnswer = 190000000000;
+    int256 private answer = 190_000_000_000;
+    int256 private prevAnswer = 190_000_000_000;
     uint256 private startedAt;
     uint256 private updatedAt;
 
@@ -55,12 +56,22 @@ contract MockAggregator is AggregatorV3Interface {
         return decimalsVal;
     }
 
-    function latestRoundData() external view override returns (uint80, int256, uint256, uint256, uint80) {
+    function latestRoundData()
+        external
+        view
+        override
+        returns (uint80, int256, uint256, uint256, uint80)
+    {
         uint256 timestamp = priceIsAlwaysUpToDate ? block.timestamp - 2 minutes : updatedAt;
         return (latestRoundId, answer, startedAt, timestamp, 0);
     }
 
-    function getRoundData(uint80) external view override returns (uint80, int256, uint256, uint256, uint80) {
+    function getRoundData(uint80)
+        external
+        view
+        override
+        returns (uint80, int256, uint256, uint256, uint80)
+    {
         uint256 timestamp = priceIsAlwaysUpToDate ? block.timestamp - 5 minutes : updatedAt;
         return (prevRoundId, prevAnswer, startedAt, timestamp, 0);
     }
