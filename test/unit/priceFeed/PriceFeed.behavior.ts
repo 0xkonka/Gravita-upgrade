e03ffe796b263cdcd61d6b9031e57dff48f48a7c
Kei <kei@tren.finance>
2024-03-22 22:01:18 +0900

    add effects to PriceFeed behavior

--- test/unit/priceFeed/PriceFeed.behavior.ts
+++ test/unit/priceFeed/PriceFeed.behavior.ts
@@ -1,14 +1,24 @@
+import shouldBehaveLikeCanSetOracle from "./effects/setOracle";
 import shouldHavePublicConstant from "./view/constants";
 import shouldBehaveLikeOwner from "./view/owner";
+import shouldHaveFetchPrice from "./view/fetchPrice";
 
 export function shouldBehaveLikePriceFeedContract(): void {
   describe("View Functions", function () {
     shouldHavePublicConstant();
 
-    describe("#owner", function () {
+    describe("#owner", function() {
       shouldBehaveLikeOwner();
     });
+
+    describe("#fetchPrice", function() {
+      shouldHaveFetchPrice();
+    })
   });
 
-  describe("Effects Functions", function () {});
+  describe("Effects Functions", function() {
+    describe("#setOracle", function () {
+      shouldBehaveLikeCanSetOracle();
+    });
+  });
 }
