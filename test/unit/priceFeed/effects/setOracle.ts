e17b36f8594a564347ffaaa34f9c487f9896e1b2
Kei <kei@tren.finance>
2024-03-26 10:18:18 +0900

    update setOracle effect

--- test/unit/priceFeed/effects/setOracle.ts
+++ test/unit/priceFeed/effects/setOracle.ts
@@ -9,15 +9,26 @@ export default function shouldBehaveLikeCanSetOracle(): void {
       isFallback: false,
     }
 
+    this.api3OracleOptions = {
+      providerType: 1, // enum ProviderType.API3
+      timeoutSeconds: 7200,
+      isEthIndexed: false,
+      isFallback: true,
+    };
+
     this.owner = this.signers.deployer;
     this.notOwner = this.signers.accounts[1];
+    this.impostor = this.signers.accounts[2]; // mock timelock
 
-    this.erc20Address = await this.testContracts.erc20.getAddress();
-    this.mockAggregatorAddress = await this.testContracts.mockAggregator.getAddress();
+    this.mockAggregator = this.testContracts.mockAggregator;
+    this.mockApi3 = this.testContracts.mockApi3;
+    this.erc20Address = await (this.testContracts.erc20).getAddress();
+    this.mockAggregatorAddress = await this.mockAggregator.getAddress();
+    this.mockApi3Address = await this.mockApi3.getAddress();
   });
 
-  context("when caller is not owner and the oracle is first set", function () {
-    it("should revert", async function () {
+  context("when the oracle is first set", function () {
+    it("should revert if caller is EOA", async function () {
       await expect(
         this.redeployedContracts.priceFeed
           .connect(this.notOwner)
@@ -34,27 +45,32 @@ export default function shouldBehaveLikeCanSetOracle(): void {
         "OwnableUnauthorizedAccount"
       );
     });
-  });
 
-  context("when caller is not owner and the oracle is not first set", function () {
-    beforeEach(async function () {
-      // set primary oracle
-      await this.redeployedContracts.priceFeed.connect(this.owner).setOracle(
-        this.erc20Address,
-        this.mockAggregatorAddress,
-        this.defaultOracleOptions.providerType,
-        this.defaultOracleOptions.timeoutSeconds,
-        this.defaultOracleOptions.isEthIndexed,
-        this.defaultOracleOptions.isFallback
+    it("should revert if fallback is true", async function () {
+      // since primary oracle is not set first
+      await expect(
+        this.redeployedContracts.priceFeed
+          .connect(this.owner)
+          .setOracle(
+            this.erc20Address,
+            this.mockApi3Address,
+            this.api3OracleOptions.providerType,
+            this.api3OracleOptions.timeoutSeconds,
+            this.api3OracleOptions.isEthIndexed,
+            this.api3OracleOptions.isFallback
+          )
+      ).to.be.revertedWithCustomError(
+        this.redeployedContracts.priceFeed,
+        "PriceFeed__ExistingOracleRequired"
       );
     });
 
-    it("should revert", async function () {
-      // oracle is not first set
-      // caller is not owner
+    it("should revert if decimals is zero", async function () {
+      await this.mockAggregator.setDecimals(0);
+
       await expect(
         this.redeployedContracts.priceFeed
-          .connect(this.notOwner)
+          .connect(this.owner)
           .setOracle(
             this.erc20Address,
             this.mockAggregatorAddress,
@@ -65,13 +81,13 @@ export default function shouldBehaveLikeCanSetOracle(): void {
           )
       ).to.be.revertedWithCustomError(
         this.redeployedContracts.priceFeed,
-        "PriceFeed__TimelockOnlyError"
+        "PriceFeed__InvalidDecimalsError"
       );
     });
-  });
 
-  context("when caller is owner and the oracle is first set", function () {
-    it("should revert if fallback is true", async function () {
+    it("should revert if chainlink price is zero", async function () {
+      await this.mockAggregator.setPrice(0);
+
       await expect(
         this.redeployedContracts.priceFeed
           .connect(this.owner)
@@ -81,29 +97,174 @@ export default function shouldBehaveLikeCanSetOracle(): void {
             this.defaultOracleOptions.providerType,
             this.defaultOracleOptions.timeoutSeconds,
             this.defaultOracleOptions.isEthIndexed,
-            !this.defaultOracleOptions.isFallback
+            this.defaultOracleOptions.isFallback
           )
       ).to.be.revertedWithCustomError(
         this.redeployedContracts.priceFeed,
-        "PriceFeed__ExistingOracleRequired"
+        "PriceFeed__InvalidOracleResponseError"
       );
     });
 
-    it("should emit NewOracleRegistered", async function () {
-      await expect(this.redeployedContracts.priceFeed.connect(this.owner).setOracle(
+    it("should revert if api3 price is zero", async function () {
+      await this.mockApi3.setValue(0);
+
+      await expect(
+        this.redeployedContracts.priceFeed
+          .connect(this.owner)
+          .setOracle(
+            this.erc20Address,
+            this.mockApi3Address,
+            this.api3OracleOptions.providerType,
+            this.api3OracleOptions.timeoutSeconds,
+            this.api3OracleOptions.isEthIndexed,
+            !this.api3OracleOptions.isFallback
+          )
+      ).to.be.revertedWithCustomError(
+        this.redeployedContracts.priceFeed,
+        "PriceFeed__InvalidOracleResponseError"
+      );
+    });
+
+    it("should set primary oracle", async function () {
+      await this.redeployedContracts.priceFeed
+        .connect(this.owner)
+        .setOracle(
+          this.erc20Address,
+          this.mockAggregatorAddress,
+          this.defaultOracleOptions.providerType,
+          this.defaultOracleOptions.timeoutSeconds,
+          this.defaultOracleOptions.isEthIndexed,
+          this.defaultOracleOptions.isFallback
+        );
+
+      const oracles = await this.redeployedContracts.priceFeed.oracles(this.erc20Address);
+      expect(oracles.oracleAddress).to.equal(this.mockAggregatorAddress);
+    });
+
+    it("should set fallback oracle", async function () {
+      // set first primary oracle
+      await this.redeployedContracts.priceFeed
+        .connect(this.owner)
+        .setOracle(
+          this.erc20Address,
+          this.mockAggregatorAddress,
+          this.defaultOracleOptions.providerType,
+          this.defaultOracleOptions.timeoutSeconds,
+          this.defaultOracleOptions.isEthIndexed,
+          this.defaultOracleOptions.isFallback
+        );
+
+      // set fallback oracle
+      await this.redeployedContracts.priceFeed
+        .connect(this.owner)
+        .setOracle(
+          this.erc20Address,
+          this.mockApi3Address,
+          this.api3OracleOptions.providerType,
+          this.api3OracleOptions.timeoutSeconds,
+          this.api3OracleOptions.isEthIndexed,
+          this.api3OracleOptions.isFallback
+        );
+      const fallback = await this.redeployedContracts.priceFeed.fallbacks(this.erc20Address);
+      expect(fallback.oracleAddress).to.equal(this.mockApi3Address);
+    });
+  });
+
+  context("when the oracle is not first set", function () {
+    beforeEach(async function () {
+      // set primary oracle as chainlink
+      await this.redeployedContracts.priceFeed.connect(this.owner).setOracle(
         this.erc20Address,
         this.mockAggregatorAddress,
         this.defaultOracleOptions.providerType,
         this.defaultOracleOptions.timeoutSeconds,
         this.defaultOracleOptions.isEthIndexed,
         this.defaultOracleOptions.isFallback
-      )).to.emit(this.redeployedContracts.priceFeed, "NewOracleRegistered")
-      .withArgs(
-        this.erc20Address,
-        this.mockAggregatorAddress,
-        this.defaultOracleOptions.isEthIndexed,
-        this.defaultOracleOptions.isFallback
       );
+
+      // set fallback oracle as api3
+      await this.redeployedContracts.priceFeed.connect(this.owner).setOracle(
+        this.erc20Address,
+        this.mockApi3Address,
+        this.api3OracleOptions.providerType,
+        this.api3OracleOptions.timeoutSeconds,
+        this.api3OracleOptions.isEthIndexed,
+        this.api3OracleOptions.isFallback
+      );
+
+      // set timelock
+      const addressesForSetAddresses = await this.utils.getAddressesForSetAddresses({
+        timelock: this.impostor,
+      });
+      await this.redeployedContracts.priceFeed.setAddresses(addressesForSetAddresses);
+    });
+
+    it("should revert if caller is EOA", async function () {
+      // caller should be a timelock
+      await expect(
+        this.redeployedContracts.priceFeed
+          .connect(this.notOwner)
+          .setOracle(
+            this.erc20Address,
+            this.mockApi3Address,
+            this.api3OracleOptions.providerType,
+            this.api3OracleOptions.timeoutSeconds,
+            this.api3OracleOptions.isEthIndexed,
+            !this.api3OracleOptions.isFallback
+          )
+      ).to.be.revertedWithCustomError(
+        this.redeployedContracts.priceFeed,
+        "PriceFeed__TimelockOnlyError"
+      );
+    });
+
+    it("should revert if caller is owner", async function () {
+      // caller should be a timelock
+      await expect(
+        this.redeployedContracts.priceFeed
+          .connect(this.owner)
+          .setOracle(
+            this.erc20Address,
+            this.mockApi3Address,
+            this.api3OracleOptions.providerType,
+            this.api3OracleOptions.timeoutSeconds,
+            this.api3OracleOptions.isEthIndexed,
+            !this.api3OracleOptions.isFallback
+          )
+      ).to.be.revertedWithCustomError(
+        this.redeployedContracts.priceFeed,
+        "PriceFeed__TimelockOnlyError"
+      );
+    });
+
+    it("should update primary oracle if caller is timelock", async function () {
+      await this.redeployedContracts.priceFeed
+        .connect(this.impostor)
+        .setOracle(
+          this.erc20Address,
+          this.mockApi3Address,
+          this.api3OracleOptions.providerType,
+          this.api3OracleOptions.timeoutSeconds,
+          this.api3OracleOptions.isEthIndexed,
+          !this.api3OracleOptions.isFallback
+        );
+      const oracle = await this.redeployedContracts.priceFeed.oracles(this.erc20Address);
+      expect(oracle.oracleAddress).to.equal(this.mockApi3Address);
+    });
+
+    it("should update fallback oracle if caller is timelock", async function () {
+      await this.redeployedContracts.priceFeed
+        .connect(this.impostor)
+        .setOracle(
+          this.erc20Address,
+          this.mockAggregatorAddress,
+          this.defaultOracleOptions.providerType,
+          this.defaultOracleOptions.timeoutSeconds,
+          this.defaultOracleOptions.isEthIndexed,
+          !this.defaultOracleOptions.isFallback
+        );
+      const fallback = await this.redeployedContracts.priceFeed.fallbacks(this.erc20Address);
+      expect(fallback.oracleAddress).to.equal(this.mockAggregatorAddress);
     });
   });
 }
