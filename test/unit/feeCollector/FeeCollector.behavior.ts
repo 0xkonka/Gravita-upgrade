e5210545e79e62e9d02db641f22d4157502e11d3
Kei <kei@tren.finance>
2024-03-27 18:58:35 +0900

    init effects test

--- test/unit/feeCollector/FeeCollector.behavior.ts
+++ test/unit/feeCollector/FeeCollector.behavior.ts
@@ -1,8 +1,13 @@
 import shouldHavePublicConstant from "./view/constants";
+import shouldBehaveLikeCanAuthorizeUpgrade from "./effects/authorizeUpgrade";
 
 export function shouldBehaveLikeFeeCollectorContract(): void {
   describe("View Functions", function () {
     shouldHavePublicConstant();
   });
-  describe("Effects Functions", function () {});
+  describe("Effects Functions", function () {
+    describe("#authorizeUpgrade", function () {
+      shouldBehaveLikeCanAuthorizeUpgrade();
+    });
+  });
 }
